20d172b70f8b9844e7fef0999af616194196a48b
takeru1201 <ldn.asktmhmh@icloud.com>
2020-07-12 22:21:36 +0900

    new:createアクション
    createアクションを導入しデータが保存できるようにした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,15 @@ class PostsController < ApplicationController
   def new 
     @post = Post.new
   end
+
+  def create
+    Post.create(post_params)
+  end
   
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
